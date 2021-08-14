8b9a2a8b8031d2544de7701255790527ac53f207
Josephcollins Udeani <C-Joseph.Collins@charter.com>
2021-08-14 07:53:47 -0500

    Completed module 5

--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using Roster.Client.Models;
 using Xamarin.Forms;
 
 namespace Roster.Client.ViewModels
@@ -18,24 +20,25 @@ namespace Roster.Client.ViewModels
                 _title = value;
                 PropertyChanged(this, new PropertyChangedEventArgs(nameof(Title)));
             }            
-        } 
+        }
+
+        public ObservableCollection<Person> Persons { get; set; }
 
         public Command UpdateApplicationCommand { get; set; }
 
         public HomeViewModel()
         {
             UpdateApplicationCommand = new Command(UpdateApplicationCommandExecute);
+            Persons = new ObservableCollection<Person>();
+            Persons.Add(new Person() { Name="Delores Feil", Company= "Legros Group"});
+            Persons.Add(new Person() { Name = "Ann Zboncak", Company = "Ledner - Ferry" });
+            Persons.Add(new Person() { Name = "Jaime Lesch", Company = "Herzog and Sons" });
+
         }
 
         private void UpdateApplicationCommandExecute()
         {
             Title = AppResource.AppNameVersion2;
         }
-            
-        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
     }
 }
