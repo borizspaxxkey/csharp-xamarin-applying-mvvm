984949a64e8e45b422e34683312a8cf4d28496b8
Josephcollins Udeani <C-Joseph.Collins@charter.com>
2021-08-14 07:13:24 -0500

    Completed module 3

--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace Roster.Client.ViewModels
 {
     public class HomeViewModel: INotifyPropertyChanged
     {
-        public string Title { get; set; } = "Roster App";
+        private string title;
+        public string Title
+        {
+            get { return AppResource.AppNameVersion1; }
+            set { title = value; NotifyPropertyChanged(); }            
+        } 
 
-        public Command UpdateApplicationCommand { get; set; }
+        #region Command
+        public ICommand UpdateApplicationCommand { get; private set; }
+        #endregion
 
         public HomeViewModel()
         {
+            UpdateApplicationCommand = new Command(() => Title = AppResource.AppNameVersion2);
+                       
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
--- Roster.Client/Views/HomeView.xaml.cs
+++ Roster.Client/Views/HomeView.xaml.cs
@@ -6,10 +6,20 @@ namespace Roster.Client.Views
     [System.ComponentModel.DesignTimeVisible(false)]
     public partial class HomeView : ContentPage
     {
+        #region Constructor
         public HomeView()
         {
             InitializeComponent();
             BindingContext = new HomeViewModel();
         }
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
     }
 }
