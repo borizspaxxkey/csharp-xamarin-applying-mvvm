936fbb587053d78576174867edaa0e8e7e289b46
Josephcollins Udeani <C-Joseph.Collins@charter.com>
2021-08-14 05:43:31 -0500

    Completed module

--- Roster.Client/Views/HomeView.xaml.cs
+++ Roster.Client/Views/HomeView.xaml.cs
@@ -5,9 +5,12 @@ namespace Roster.Client.Views
     [System.ComponentModel.DesignTimeVisible(false)]
     public partial class HomeView : ContentPage
     {
+        object v = new { Title = 108 };
+
         public HomeView()
         {
             InitializeComponent();
+            BindingContext = v;
         }
     }
 }
